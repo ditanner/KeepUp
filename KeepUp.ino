1e2bb2318e77a8e9efcbe4bb45902ae63fe54747
ditanner <github@darrentanner.com>
2022-01-05 02:10:55 +0000

    Update KeepUp.ino
    Added scoring

--- KeepUp.ino
+++ KeepUp.ino
@@ -1,22 +1,31 @@
 
 #include "Serial.h"
 
-enum signalStates {SETUP, GAME, INERT, RESOLVE};
+enum signalStates {SETUP, GAME, GAMEOVER, LIFELOSS, INERT, RESOLVE};
 byte signalState = INERT;
 byte gameMode = SETUP;
 
 enum games {WAIT, FAIL, ROTATE_CW, ROTATE_ACW, FLIP, DOUBLE_C, TRIPLE_C};
 byte game = WAIT;
 
-enum blinkModes {UNUSED, GAMEPLAYER, SCORER};
+enum blinkModes {UNUSED, GAMEPLAYER, CONNECTOR, SPUR, CONTROLLER};
 byte blinkMode = UNUSED;
+
+enum gameOverStates {GO_WIN, GO_LOSS};
+byte gameOverState = GO_WIN;
 
 byte connectedFace = 7;
 byte targetFace = 7;
 
+byte score = 0;
+
+int gameOverFace = -1;
+bool gameOver = false;
+bool gameOverPublished = false;
+
 #define PULSE_LENGTH 750
 
-ServicePortSerial sp;
+//ServicePortSerial sp;
 
 Timer gameTimer;
 #define gameLength 5000
@@ -27,10 +36,17 @@ Timer startTimer;
 void setup() {
   randomize();
   setColor(OFF);
-  sp.begin();
+  setValueSentOnAllFaces(INERT<<2);
+//  sp.begin();
 }
 
 void loop() {
+
+  if (CONTROLLER == blinkMode && INERT == signalState && gameOver && !gameOverPublished) {
+    signalState = GAMEOVER;
+    gameMode = GAMEOVER;
+    gameOverPublished = true;
+  }
 
   if (signalState < INERT) {
     sendLoop();
@@ -40,44 +56,54 @@ void loop() {
     resolveLoop();
   }
 
-  switch (gameMode) {
-    case SETUP:
-      setupLoop();
-      break;
-    case GAME:
-      gameLoop();
-      if (GAMEPLAYER == blinkMode) {
-        switch (game) {
-          case WAIT:
-            setColor(GREEN);
-            break;
-          case FAIL:
-            setColor(RED);
-            break;
-          case ROTATE_CW:
-            drawRotate(true);
-            break;
-          case ROTATE_ACW:
-            drawRotate(false);
-            break;
-          case FLIP:
-            drawFlip();
-            break;
-          //          case SINGLE_C:
-          //            drawSingleClick();
-          //            break;
-          case DOUBLE_C:
-            drawDoubleClick();
-            break;
-          case TRIPLE_C:
-            drawTripleClick();
-            break;
-        }
-      } else {
-        setColor(OFF);
+  if (SETUP == gameMode) {
+    setupLoop();
+  } else if (GAME == gameMode) {
+
+    gameLoop();
+    if (GAMEPLAYER == blinkMode) {
+      switch (game) {
+        case WAIT:
+          setColor(GREEN);
+          break;
+        case FAIL:
+          setColor(RED);
+          break;
+        case ROTATE_CW:
+          drawRotate(true);
+          break;
+        case ROTATE_ACW:
+          drawRotate(false);
+          break;
+        case FLIP:
+          drawFlip();
+          break;
+        //          case SINGLE_C:
+        //            drawSingleClick();
+        //            break;
+        case DOUBLE_C:
+          drawDoubleClick();
+          break;
+        case TRIPLE_C:
+          drawTripleClick();
+          break;
       }
-      break;
+    } else if (GAMEPLAYER < blinkMode) {
+      drawScore();
+      if (LIFELOSS == signalState) {
+        setColor(RED);
+      }
+    } else {
+      setColor(OFF);
+    }
+  } else if (GAMEOVER == gameMode) {
+    if (CONTROLLER == blinkMode) {
+      gameLoop();
+    }
+    drawGameOver();
+
   }
+
 
   //dump button data
   buttonSingleClicked();
@@ -87,8 +113,23 @@ void loop() {
 
   byte sendData = 0;
 
-  sendData = (signalState << 3);
-  setValueSentOnAllFaces(sendData);
+  sendData = (signalState << 2);
+  //sendData = sendData + gameMode
+  if (GAMEOVER == signalState) {
+    if (CONTROLLER == blinkMode) {
+      FOREACH_FACE(f) {
+        if (f == gameOverFace) {
+          setValueSentOnFace(sendData + 1, f);
+        } else {
+          setValueSentOnFace(sendData, f);
+        }
+      }
+    } else {
+      setValueSentOnAllFaces(sendData + gameOverState);
+    }
+  } else {
+    setValueSentOnAllFaces(sendData);
+  }
 
 }
 
@@ -96,42 +137,80 @@ void loop() {
 void inertLoop() {
   bool sendReceived = false;
   byte val = 0;
+  byte gameOverCount = 0;
   FOREACH_FACE(f) {
     if ( !sendReceived) {
       if (!isValueReceivedOnFaceExpired(f) ) {//a neighbor!
         val = getSignalState(getLastValueReceivedOnFace(f));
         if (val < INERT ) {//a neighbor saying SEND!
-          sp.print("SEND Received - moving to mode ");
-          sp.println(val);
-          switch (val) {
-            case SETUP:
-              signalState = val;
-              gameMode = val;
-              connectedFace = 7;
-              targetFace = 7;
+//          sp.print("SEND Received - moving to mode ");
+//          sp.println(val);
+          if (SETUP == val) {
+            gameOver = false;
+            gameOverPublished = false;
+            gameOverState = 0;
+            score = 0;
+            signalState = val;
+            gameMode = val;
+            connectedFace = 7;
+            targetFace = 7;
+            gameTimer.never();
+            startTimer.never();
+
+          } else if ( GAME == val) {
+            byte neighbours = 0;
+            FOREACH_FACE(d) {
+              if (!isValueReceivedOnFaceExpired(d)) {
+                neighbours++;
+              }
+            }
+            signalState = val;
+            gameMode = val;
+            if (1 == neighbours) {
+              blinkMode = GAMEPLAYER;
+              connectedFace = f;
+              startTimer.set(startMultiplier * (random(4) + 2));
+              game = WAIT;
+              //              sp.println("game is 0");
               gameTimer.never();
-              startTimer.never();
-              break;
-            case GAME:
-              byte neighbours = 0;
-              FOREACH_FACE(f) {
-                if (!isValueReceivedOnFaceExpired(f)) {
-                  neighbours++;
-                }
-              }
-              signalState = val;
-              gameMode = val;
-              if (1 == neighbours) {
-                blinkMode = GAMEPLAYER;
-                connectedFace = f;
-                startTimer.set(startMultiplier * (random(4) + 2));
-                game = WAIT;
-                sp.println("game is 0");
-                gameTimer.never();
+            } else if (2 == neighbours) {
+              blinkMode = CONNECTOR;
+              connectedFace = f;
+            } else if (3 == neighbours) {
+              blinkMode = SPUR;
+              connectedFace = f;
+            }
+
+          }
+          else if ( LIFELOSS == val) {
+            //            sp.println("HELLO!!!");
+            if (CONTROLLER == blinkMode) {
+              //              sp.println("Moving to RESOLVE");
+              gameOverCount++;
+              if (gameOverCount > 1) {
+                gameOverFace = -1; //it's a tie - keep going
+                signalState = RESOLVE;
+                gameOver = false;
               } else {
-                blinkMode = SCORER;
+                gameOverFace = f;
+                signalState = RESOLVE;
+                gameOver = true;
               }
-              break;
+              //GAMEOVER!!!
+            } else {
+              //              sp.println("Moving to LIFELOSS");
+              if (score < 6) {
+                score++;
+                signalState = RESOLVE;
+              } else {
+                signalState = val;
+              }
+            }
+
+          } else if (GAMEOVER == val) {
+            gameOverState = getPayload(getLastValueReceivedOnFace(f));
+            signalState = GAMEOVER;
+            gameMode = GAMEOVER;
           }
           sendReceived = true;
         }
@@ -145,13 +224,28 @@ void sendLoop() {
   //look for neighbors who have not heard the GO news
   FOREACH_FACE(f) {
     if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
-      if (getSignalState(getLastValueReceivedOnFace(f)) == INERT) {//This neighbor doesn't know it's SEND time. Stay in SEND
+      byte val = getSignalState(getLastValueReceivedOnFace(f));
+      if (val == INERT) {//This neighbor doesn't know it's SEND time. Stay in SEND
         allSend = false;
+      } else if (GAMEOVER == val && GAMEOVER != gameMode) {
+        allSend = false;
+        gameOverState = getPayload(getLastValueReceivedOnFace(f));
+        signalState = GAMEOVER;
+        gameMode = GAMEOVER;
+      } else {
+        if (INERT > signalState && INERT > val && val != signalState) {
+          //conflict!!!
+          if (signalState > val) {
+//            sp.println("CONFLICT!");
+            inertLoop();
+            return;
+          }
+        }
       }
     }
   }
   if (allSend) {
-    sp.println("All Resolve");
+//    sp.println("All Resolve");
     signalState = RESOLVE;
   }
 }
@@ -162,13 +256,20 @@ void resolveLoop() {
   //look for neighbors who have not moved to RESOLVE
   FOREACH_FACE(f) {
     if (!isValueReceivedOnFaceExpired(f)) {//a neighbor!
-      if (getSignalState(getLastValueReceivedOnFace(f)) < INERT) {//This neighbor isn't in RESOLVE. Stay in RESOLVE
+      byte val = getSignalState(getLastValueReceivedOnFace(f));
+      if (val < INERT) {//This neighbor isn't in RESOLVE. Stay in RESOLVE
         allResolve = false;
+      }
+      if (GAMEOVER == val && GAMEOVER != gameMode) {
+        allResolve = false;
+        gameOverState = getPayload(getLastValueReceivedOnFace(f));
+        signalState = GAMEOVER;
+        gameMode = GAMEOVER;
       }
     }
   }
   if (allResolve) {
-    sp.println("All INERT");
+//    sp.println("All INERT");
     signalState = INERT;
   }
 }
@@ -177,7 +278,7 @@ void setupLoop() {
   if (buttonDoubleClicked()) {
     signalState = GAME;
     gameMode = GAME;
-    blinkMode = SCORER;
+    blinkMode = CONTROLLER;
   }
 }
 
@@ -192,8 +293,8 @@ void gameLoop() {
     if (startTimer.isExpired()) {
       gameTimer.set(gameLength);
       game = random(4) + 2;
-      sp.print("180 game is ");
-      sp.println(game);
+      //     sp.print("180 game is ");
+      //     sp.println(game);
       startTimer.never();
       connectedFace = currentFace;
 
@@ -215,15 +316,16 @@ void gameLoop() {
     if (gameTimer.isExpired()) {
       if (FAIL != game) {
         game = FAIL;
-        sp.print("203 game is ");
-        sp.println(game);
+        //        sp.print("203 game is ");
+        //        sp.println(game);
         gameTimer.set(1000);
+        signalState = LIFELOSS;
         return;
       }
       startTimer.set(startMultiplier * (random(4) + 1));
       game = WAIT;
-      sp.print("210 game is ");
-      sp.println(game);
+      //      sp.print("210 game is ");
+      //      sp.println(game);
       gameTimer.never();
       return;
     }
@@ -239,15 +341,15 @@ void gameLoop() {
 
       if (currentFace != connectedFace && currentFace != targetFace && currentFace != 7) {
         game = FAIL;
-        sp.print("224 game is ");
-        sp.println(game);
+        //        sp.print("224 game is ");
+        //        sp.println(game);
 
         gameTimer.set(1000);
         return;
       } else if (game < 4 && (clicks > 0 || dblClick || sglClick)) {
         game = FAIL;
-        sp.print("231 game is ");
-        sp.println(game);
+        //       sp.print("231 game is ");
+        //        sp.println(game);
 
         gameTimer.set(1000);
         return;
@@ -257,8 +359,8 @@ void gameLoop() {
           if (!isValueReceivedOnFaceExpired(targetFace)) {
             startTimer.set(startMultiplier * (random(4) + 1));
             game = WAIT;
-            sp.print("242 game is ");
-            sp.println(game);
+            //            sp.print("242 game is ");
+            //           sp.println(game);
 
             gameTimer.never();
           }
@@ -267,8 +369,8 @@ void gameLoop() {
           if (!isValueReceivedOnFaceExpired(targetFace)) {
             startTimer.set(startMultiplier * (random(4) + 1));
             game = WAIT;
-            sp.print("252 game is ");
-            sp.println(game);
+            //            sp.print("252 game is ");
+            //            sp.println(game);
 
             gameTimer.never();
           }
@@ -277,8 +379,8 @@ void gameLoop() {
           if (!isValueReceivedOnFaceExpired(targetFace)) {
             startTimer.set(startMultiplier * (random(4) + 1));
             game = WAIT;
-            sp.print("261 game is ");
-            sp.println(game);
+            //            sp.print("261 game is ");
+            //            sp.println(game);
 
             gameTimer.never();
           }
@@ -287,8 +389,8 @@ void gameLoop() {
           if (dblClick) {
             startTimer.set(startMultiplier * (random(4) + 1));
             game = WAIT;
-            sp.print("271 game is ");
-            sp.println(game);
+            //            sp.print("271 game is ");
+            //            sp.println(game);
 
             gameTimer.never();
           }
@@ -297,8 +399,8 @@ void gameLoop() {
           if (3 == clicks) {
             startTimer.set(startMultiplier * (random(4) + 1));
             game = WAIT;
-            sp.print("282 game is ");
-            sp.println(game);
+            //            sp.print("282 game is ");
+            //            sp.println(game);
 
             gameTimer.never();
           }
@@ -306,12 +408,17 @@ void gameLoop() {
     }
   } else {
     if (buttonDoubleClicked()) {
+      gameOver = false;
+      gameOverPublished = false;
+      gameOverState = 0;
+      score = 0;
       signalState = SETUP;
       gameMode = SETUP;
       connectedFace = 7;
       targetFace = 7;
       gameTimer.never();
       startTimer.never();
+
     }
   }
 }
@@ -322,11 +429,11 @@ byte stepOverFace(byte face, byte steps) {
 }
 
 byte getSignalState(byte data) {
-  return ((data >> 3) );
+  return ((data >> 2) );
 }
 
 byte getPayload(byte data) {
-  return (data & 7);
+  return (data & 3);
 }
 
 void drawRotate(bool clockwise) {
@@ -417,5 +524,25 @@ void drawTripleClick() {
     //    setColor(BLUE);
     setColorOnFace(BLUE, 4);
     setColorOnFace(BLUE, 5);
+  }
+}
+
+void drawScore() {
+  setColor(OFF);
+  for (byte i = 0; i < score; i++) {
+    setColorOnFace(RED, i);
+  }
+}
+
+void drawGameOver() {
+  if (CONTROLLER == blinkMode) {
+    setColor(OFF);
+    setColorOnFace(RED, gameOverFace);
+  } else {
+    if (GO_WIN == gameOverState) {
+      setColor(GREEN);
+    } else {
+      setColor(RED);
+    }
   }
 }
